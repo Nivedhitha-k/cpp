7889e76925c6113c62a93faaa01a5da0256b6b76
Nivedhitha k <88976284+Nivedhitha-k@users.noreply.github.com>
2023-01-20 09:17:07 +0530

    Create search insert.cpp

---
+++ search insert.cpp
@@ -0,0 +1,8 @@
+int searchInsert(vector<int>& nums, int target) {
+       // int pos=0;
+       for(int i=0;i<nums.size();i++){
+         if(nums[i]>=target)
+         return i;     
+       }
+       return nums.size(); 
+    }
